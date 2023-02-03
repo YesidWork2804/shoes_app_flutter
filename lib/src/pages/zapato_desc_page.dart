80556cd9cbfa96e7e142f0c7a94ff813fd759b1f
Yesid200228 <yesidwork2804@gmail.com>
2023-02-03 13:04:24 -0500

    Modificacion del archivo Readme

--- lib/src/pages/zapato_desc_page.dart
+++ lib/src/pages/zapato_desc_page.dart
@@ -5,11 +5,9 @@ import 'package:contador/src/widgets/custom_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class ZapatoDescPage  extends StatelessWidget {
-
+class ZapatoDescPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     cambiarStatusLight();
 
     return Scaffold(
@@ -17,40 +15,42 @@ class ZapatoDescPage  extends StatelessWidget {
         children: [
           Stack(
             children: [
-              Hero(tag: 'zapato-1', child: ZapatoSizePreview(fullScrren: false)),
+              Hero(
+                  tag: 'zapato-1', child: ZapatoSizePreview(fullScrren: false)),
               Positioned(
-                top: 80,
-                child: FloatingActionButton(
-                  child: Icon(Icons.chevron_left, color: Colors.white,size: 60,),
-                  onPressed: (){
-                    cambiarStatusDark();
-                    Navigator.pop(context);
-                  },
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  highlightElevation: 0,
-                )
-              )
+                  top: 80,
+                  child: FloatingActionButton(
+                    child: Icon(
+                      Icons.chevron_left,
+                      color: Colors.white,
+                      size: 60,
+                    ),
+                    onPressed: () {
+                      cambiarStatusDark();
+                      Navigator.pop(context);
+                    },
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    highlightElevation: 0,
+                  ))
             ],
           ),
-          Expanded( 
+          Expanded(
             child: SingleChildScrollView(
               child: Column(
                 children: [
                   ZapatoDescripcion(
-                  titulo: 'Nike Air Max 720',
-                  descripcion: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
+                    titulo: 'Nike Air Max 720',
+                    descripcion:
+                        "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
                   ),
                   _MontoBuyNow(),
                   _ColoresYMas(),
-
                   _BotonesLikeCartSettings()
-
                 ],
               ),
             ),
           )
-
         ],
       ),
     );
@@ -58,7 +58,6 @@ class ZapatoDescPage  extends StatelessWidget {
 }
 
 class _BotonesLikeCartSettings extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -67,19 +66,19 @@ class _BotonesLikeCartSettings extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _BotonSombreado(Icon(Icons.star,color: Colors.red,size: 25)),
-          _BotonSombreado(Icon(Icons.add_shopping_cart,color: Colors.grey.withOpacity(0.4),size: 25)),
-          _BotonSombreado(Icon(Icons.settings,color: Colors.grey.withOpacity(0.4),size: 25))
-
+          _BotonSombreado(Icon(Icons.star, color: Colors.red, size: 25)),
+          _BotonSombreado(Icon(Icons.add_shopping_cart,
+              color: Colors.grey.withOpacity(0.4), size: 25)),
+          _BotonSombreado(Icon(Icons.settings,
+              color: Colors.grey.withOpacity(0.4), size: 25))
         ],
       ),
     );
   }
 }
 
-class _BotonSombreado extends StatelessWidget { 
-
- final Icon icon;
+class _BotonSombreado extends StatelessWidget {
+  final Icon icon;
 
   const _BotonSombreado(this.icon);
 
@@ -90,38 +89,51 @@ class _BotonSombreado extends StatelessWidget {
       width: 55,
       height: 55,
       decoration: BoxDecoration(
-        color: Colors.white,
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow( color: Colors.black12 ,spreadRadius: -5,blurRadius: 20,offset: Offset( 0,10))
-        ]
-
-      ),
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12,
+                spreadRadius: -5,
+                blurRadius: 20,
+                offset: Offset(0, 10))
+          ]),
     );
   }
 }
 
 class _ColoresYMas extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Row(
         children: [
-
           Expanded(
             child: Stack(
               children: [
-              Positioned(left: 90, child: _BotonColor(Color(0xff364D56),4,'assets/imgs/negro.png')),
-              Positioned(left: 60, child: _BotonColor(Color(0xff2099F1),3,'assets/imgs/azul.png')),
-              Positioned(left: 30, child: _BotonColor(Color(0xffFFAD29),2,'assets/imgs/amarillo.png')),
-              _BotonColor(Color(0xffC6D642),1, 'assets/imgs/verde.png'),
+                Positioned(
+                    left: 90,
+                    child: _BotonColor(
+                        Color(0xff364D56), 4, 'assets/imgs/negro.png')),
+                Positioned(
+                    left: 60,
+                    child: _BotonColor(
+                        Color(0xff2099F1), 3, 'assets/imgs/azul.png')),
+                Positioned(
+                    left: 30,
+                    child: _BotonColor(
+                        Color(0xffFFAD29), 2, 'assets/imgs/amarillo.png')),
+                _BotonColor(Color(0xffC6D642), 1, 'assets/imgs/verde.png'),
               ],
             ),
           ),
-         
-          BotonNaranja(texto: 'More related items', alto: 30,ancho: 170,color: Color(0xffFFC675),)
+          BotonNaranja(
+            texto: 'More related items',
+            alto: 30,
+            ancho: 170,
+            color: Color(0xffFFC675),
+          )
         ],
       ),
     );
@@ -129,12 +141,11 @@ class _ColoresYMas extends StatelessWidget {
 }
 
 class _BotonColor extends StatelessWidget {
-
   final Color color;
   final int index;
   final String urlImagen;
 
-  const _BotonColor(this.color, this.index,this.urlImagen);
+  const _BotonColor(this.color, this.index, this.urlImagen);
 
   @override
   Widget build(BuildContext context) {
@@ -142,17 +153,14 @@ class _BotonColor extends StatelessWidget {
       delay: Duration(milliseconds: this.index * 100),
       duration: Duration(milliseconds: 300),
       child: GestureDetector(
-        onTap: (){
-          final zapatoModel = Provider.of<ZapatoModel>(context,listen: false);
+        onTap: () {
+          final zapatoModel = Provider.of<ZapatoModel>(context, listen: false);
           zapatoModel.assetImage = this.urlImagen;
         },
         child: Container(
           width: 45,
           height: 45,
-          decoration: BoxDecoration(
-            color: color,
-            shape: BoxShape.circle
-          ),
+          decoration: BoxDecoration(color: color, shape: BoxShape.circle),
         ),
       ),
     );
@@ -160,22 +168,25 @@ class _BotonColor extends StatelessWidget {
 }
 
 class _MontoBuyNow extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Container(
-        margin: EdgeInsets.only(top: 20,bottom: 20),
+        margin: EdgeInsets.only(top: 20, bottom: 20),
         child: Row(
           children: [
-            Text('\$180',style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold)),
+            Text('\$180',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
             Spacer(),
             Bounce(
-              delay: Duration(seconds: 1),
-              from: 8,
-              child: BotonNaranja(texto: 'Buy Now',ancho: 120,alto: 40,)
-            )
+                delay: Duration(seconds: 1),
+                from: 8,
+                child: BotonNaranja(
+                  texto: 'Buy Now',
+                  ancho: 120,
+                  alto: 40,
+                ))
           ],
         ),
       ),
